45e729142085c12c0e42578707e29089ef8ba6a7
elialdinger <49462755+elialdinger@users.noreply.github.com>
2019-12-02 10:53:26 -0700

    added unity files for database connection and for photon network

---
+++ PUN2Test/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+    private float x;
+    private Rigidbody2D rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal");
+
+        transform.position += (Vector3)new Vector2(x*speed*Time.deltaTime,0);
+    }
+
+    private void FixedUpdate()
+    {
+        if(Input.GetButtonDown("Jump"))
+        {
+            rigidbody.AddForce(transform.up* jumpForce, ForceMode2D.Impulse);
+        }
+    }
+}
---
+++ SQLTest/Assets/WebTest.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebTest : MonoBehaviour
+{
+     IEnumerator Start()
+     {
+          WWW request = new WWW("http://localhost/sqlconnect/webtest.php");
+          yield return request;
+          string[] webResults = request.text.Split('\t');
+          Debug.Log(webResults[0]);
+          int.Parse(webResults[1]);
+     }
+}
